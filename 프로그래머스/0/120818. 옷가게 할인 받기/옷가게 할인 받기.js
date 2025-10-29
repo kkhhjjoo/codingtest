a96c7a9911791762e0b5dbd9d00e6fd257f34422
hyunjoo <94750692+kkhhjjoo@users.noreply.github.com>
2025-10-29 20:27:24 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,12 @@
+function solution(price) {
+  if (price >= 500000) {
+    return Math.floor(price * 0.8);  // 20% 할인
+  } else if (price >= 300000) {
+    return Math.floor(price * 0.9);  // 10% 할인
+  } else if (price >= 100000) {
+    return Math.floor(price * 0.95); // 5% 할인
+  } else {
+    return price; // 할인 없음
+  }
+}
+solution(150000);
