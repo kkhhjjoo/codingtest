1bbef70144f716ba3f1e38695833e2acfac3f8eb
hyunjoo <94750692+kkhhjjoo@users.noreply.github.com>
2025-10-30 17:15:53 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.js
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -1,6 +1,7 @@
 function solution(n) {
     for (let i = 1; i <= n; i++) {
-        if (i * i === n) return 1
-        if (i * i > n) return 2
+        if (i * i === n) return 1   // i^2가 n과 같으면 완전제곱수
+        if (i * i > n) return 2     // i^2가 n을 넘어가면 더 이상 같아질 수 없음 → 완전제곱수 아님
     }
 }
+solution(144);
